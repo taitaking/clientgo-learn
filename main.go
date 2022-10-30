aa3522aaed860fe5380650a8ad208bd1e834c91a
zhualong <zhualong@hrfax.cn>
2022-10-30 20:17:03 +0800

    添加代码

--- main.go
+++ main.go
@@ -5,6 +5,9 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	v1 "k8s.io/api/apps/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/client-go/tools/cache"
 	"time"
 )
 
@@ -47,4 +50,62 @@ func main() {
 	//	fmt.Println("delete pod failed ：%s", err.Error())
 	//}
 
+	informer := kc.DeploymentInformer.Informer()
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			deploy := obj.(*v1.Deployment)
+			fmt.Println("add a deployment:", deploy.Name)
+		},
+		DeleteFunc: func(obj interface{}) {
+			deploy := obj.(*v1.Deployment)
+			fmt.Println("delete a deployment:", deploy.Name)
+		},
+		UpdateFunc: func(old, new interface{}) {
+			oldDeploy := old.(*v1.Deployment)
+			newDeploy := new.(*v1.Deployment)
+			fmt.Println("update deployment:", oldDeploy.Name, newDeploy.Name)
+		},
+	})
+
+	deploymentNamespaceLister := kc.DeploymentLister.Deployments("default")
+	list, err := deploymentNamespaceLister.List(labels.Everything())
+	if err != nil {
+		return
+	}
+
+	for index, deployment := range list {
+		bytes, err := json.Marshal(deployment)
+		if err != nil {
+			return
+		}
+		fmt.Println(fmt.Printf("顺序:%s---deployment:%s\n", index, string(bytes)))
+	}
+
+	pods, err := kc.PodLister.List(labels.Everything())
+	if err != nil {
+		return
+	}
+
+	for i, v := range pods {
+		bytes, err := json.Marshal(v)
+		if err != nil {
+			return
+		}
+		fmt.Println(fmt.Printf("pod顺序:%i---deployment:%s\n", i, string(bytes)))
+	}
+
+	labeles := make(map[string]string, 1)
+	labeles["control-plane"] = "controller-manager"
+	selector := labels.SelectorFromSet(labeles)
+	services, err := kc.ServiceInformer.Lister().Services("nodedemo-system").List(selector)
+	if err != nil {
+		return
+	}
+	for k, service := range services {
+		bytes, err := json.Marshal(service)
+		if err != nil {
+			return
+		}
+		fmt.Println(fmt.Printf("service key:%i---service:%s\n", k, string(bytes)))
+	}
 }
